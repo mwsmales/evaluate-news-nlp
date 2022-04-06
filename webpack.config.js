6a8f51480ac8668e3130c759406bede02b349f21
Michael S <michael.smales@gmail.com>
2022-04-06 11:17:05 -0500

    add webpack-dev-server and clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require("path")
 const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
     /* stats: 'verbose', */
     module: {
         rules: [
@@ -20,6 +24,12 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }), 
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
     ],
     output: {
